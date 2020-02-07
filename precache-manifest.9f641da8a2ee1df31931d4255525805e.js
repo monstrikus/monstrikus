890babf7e286d6d7b3e17e0065620a4b0a6bbc04
monstrikus <kalugin666@yandex.ru>
2020-02-07 20:30:58 +0300

    deploy

---
+++ precache-manifest.9f641da8a2ee1df31931d4255525805e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da194168076486665426",
+    "url": "/my-project/css/app.0e88525e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/my-project/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "31c5c28da3be716825b250edc0d04816",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "ec5b737bfc870556a002",
+    "url": "/my-project/js/about.3a7b5dcb.js"
+  },
+  {
+    "revision": "da194168076486665426",
+    "url": "/my-project/js/app.b1384605.js"
+  },
+  {
+    "revision": "6f93fb2e5f18372932d9",
+    "url": "/my-project/js/chunk-vendors.b42a6c66.js"
+  },
+  {
+    "revision": "f542bf4e7df044cb72378e97c4c3ad1a",
+    "url": "/my-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-project/robots.txt"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,3 @@
+importScripts("/my-project/precache-manifest.9f641da8a2ee1df31931d4255525805e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+
